5aefd2758d25cc0ae932692c8bd563f46e650bc6
Zoltan Vegh <zoli0829@gmail.com>
2025-03-05 17:59:31 +0000

    init project

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Zoltan Vegh on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather_appTests/weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appTests.swift
+//  weather_appTests
+//
+//  Created by Zoltan Vegh on 05/03/2025.
+//
+
+import Testing
+@testable import weather_app
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
