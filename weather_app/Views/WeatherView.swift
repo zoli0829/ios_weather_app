8ea738c0b9af81f294b8dfcbfeba468df10ade04
Zoltan Vegh <zoli0829@gmail.com>
2025-03-10 16:18:03 +0000

    finished

--- weather_app/Views/WeatherView.swift
+++ weather_app/Views/WeatherView.swift
@@ -11,7 +11,91 @@ struct WeatherView: View {
     var weather: ResponseBody
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            VStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feels_like.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL("https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo:"thermometer", name:"Min temp", value: weather.main.tempMin.roundDouble() + "°")
+                        
+                        Spacer()
+                        
+                        WeatherRow(logo:"thermometer", name:"Max temp", value: weather.main.tempMax.roundDouble() + "°")
+                    }
+                    
+                    HStack {
+                        WeatherRow(logo:"wind", name:"Wind speed", value: weather.wind.speed.roundDouble() + "m/s")
+                        
+                        Spacer()
+                        
+                        WeatherRow(logo:"humidity", name:"Humidity", value: weather.main.humidity.roundDouble() + "%")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.662, saturation: 0.932, brightness: 0.536))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.662, saturation: 0.932, brightness: 0.536))
+        .preferredColorScheme(.dark)
     }
 }
 
