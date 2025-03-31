c5ca5268e8ecfc0e470008965bc6b661d32445d4
Zoltan Vegh <zoli0829@gmail.com>
2025-03-31 17:52:27 +0100

    day 13

---
+++ weather_app/Other/100daysOfSwiftUI/13.playground/Contents.swift
@@ -0,0 +1,160 @@
+import Cocoa
+
+protocol Vehicle {
+    var name: String {get}
+    var currentPassengers: Int {get set}
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+protocol CanBeElectric {
+    
+}
+
+struct Car: Vehicle, CanBeElectric {
+    let name = "Car"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distance: Int) {
+        print("Im driving \(distance) km")
+    }
+    
+    func openSunroof() {
+        print("Its a nice day.")
+    }
+}
+
+struct Bicycle: Vehicle {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    
+    func travel(distance: Int) {
+        print("I am cycling \(distance)km")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("Thats too slow!")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+getTravelEstimates(using: [car, bike], distance: 120)
+
+
+// How to use opaque return types
+//
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+func getRandomBool() -> some Equatable {
+    Bool.random()
+}
+
+print(getRandomNumber() == getRandomNumber())
+
+
+// Extensions
+//
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+var quote = "   The truth is rarely pure and never simple   "
+let trimmed = quote.trim()
+
+let lyrics = """
+But I keep cruising
+Cant stop wont stop moving
+Its like I got this music in my mind.
+"""
+
+print(lyrics.lines.count)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+
+    // If we define a custom initializer, Swift will disable the member wise initializer
+}
+
+// If we extend it we can use both initializer
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+
+
+// How to create and use protocol extensions
+//
+// Collection is Sets Arrays Dictionaries
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+// Add a default implementation of sayHello()
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
