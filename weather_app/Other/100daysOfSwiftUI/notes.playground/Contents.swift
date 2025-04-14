f82ad7dab59166bc85965be74675ec60fc881c41
zoli0829 <zoli0829@gmail.com>
2025-04-14 09:00:53 +0100

    Update Contents.swift

--- weather_app/Other/100daysOfSwiftUI/notes.playground/Contents.swift
+++ weather_app/Other/100daysOfSwiftUI/notes.playground/Contents.swift
@@ -21,4 +21,32 @@ func someDates() {
 
 Text(Date.now.formatted(date: .long, time: .shortened))
 
-// 
+// url
+
+func testBundles() {
+    if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
+        if let fileContent = try? String(contentsOf: fileURL) {
+            // we loaded the file into a string
+        }
+    }
+}
+
+// Check strings
+func testStrings() {
+    let input = "a b c"
+    let letters = input.components(separatedBy: " ")
+    let letter = letters.randomElement()
+    let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+func testStrings2() {
+    let word = "swift"
+    let checker = UITextChecker()
+    
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    
+    let allGood = misspelledRange.location == NSNotFound
+}
+
+//
